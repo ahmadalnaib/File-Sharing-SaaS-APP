45771b2e826478d6e36f35d45aa5c602f2b14ba1
ahmedhabeeb73 <alnaib888@gmail.com>
2021-02-20 19:18:44 +0100

    scaffolding and navigation

--- hiuploadclient/src/views/Home.vue
+++ hiuploadclient/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="home">
-   Home
+  <div class="p-12 rounded-lg bg-gray-200 text-center">
+   <h1 class="text-3xl font-medium mb-3 text-gray-800">Hi-Upload 🚀</h1>
+    <p class="text-gray-500">Instantly upload files, share them with anyone.</p>
   </div>
 </template>
 
