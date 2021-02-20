225730d454d6e9626fda312684ceffb1e1b579de
ahmedhabeeb73 <alnaib888@gmail.com>
2021-02-20 19:24:52 +0100

    add navigation component

--- hiuploadclient/src/App.vue
+++ hiuploadclient/src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="w-full md:w-8/12 lg:w-6/12 mx-auto px-6 mt-6 md:mt-20">
-   <header class="flex flex-wrap md:flex-nowrap items-center justify-between mt-6 ">
-     <ul class="flex items-center">
-       <li><router-link class="text-sm inline-block p-3 text-gray-800" :to="{name:'home'}">Home</router-link></li>
-       <li><a class="text-sm inline-block p-3 text-gray-800"  href="">Your files</a></li>
-     </ul>
-
-     <ul class="flex items-center">
-       <li><router-link class="text-sm inline-block p-3 text-gray-800" :to="{name:'login'}">Login</router-link></li>
-       <li><a class="text-sm inline-block p-3 text-gray-800"  href="">Create account</a></li>
-     </ul>
-   </header>
-
+<app-nav/>
     <router-view/>
   </div>
 
 </template>
+
+
+<script>
+import AppNav from '@/components/AppNav'
+export default {
+components:{
+  AppNav
+}
+}
+</script>
 
 
 <style src="./assets/styles/app.css"/>
---
+++ hiuploadclient/src/components/AppNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="flex flex-wrap md:flex-nowrap items-center justify-between mt-6 ">
+    <ul class="flex items-center">
+      <li><router-link class="text-sm inline-block p-3 text-gray-800" :to="{name:'home'}">Home</router-link></li>
+      <li><a class="text-sm inline-block p-3 text-gray-800"  href="">Your files</a></li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li><router-link class="text-sm inline-block p-3 text-gray-800" :to="{name:'login'}">Login</router-link></li>
+      <li><a class="text-sm inline-block p-3 text-gray-800"  href="">Create account</a></li>
+    </ul>
+  </header>
+</template>
